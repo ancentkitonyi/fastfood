f1cfa086d47baee1223c612a84dbbb925b339ee8
Ancent Dreezy <ancentkitonyi.m@gmail.com>
2021-06-07 12:58:07 +0300

    index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('header.php'); ?>
+<body>
+	<?php include('navbar.php'); ?>
+	<?php include('carousel.php'); ?>
+	<?php include('about.php'); ?>
+    <?php include('menu.php'); ?>
+    <?php include('team.php'); ?>
+    <?php include('contact.php'); ?>
+</body>
+</html>
